dc7b496be279c3863ffbbdab289ba9b6a5342226
coder-1990 <asrorakmalovich@gmail.com>
2021-09-07 17:03:12 +0500

    fix bugs and add template

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
 // Kontaktlarni joylash uchun bo'sh array ochamiz
 var contacts = [];
 
-
 // DOM
 var elNewContactForm = document.querySelector('.js-new-contact-form');
 
 var elContacts = document.querySelector('.contacts');
 var elContactsList = elContacts.querySelector('.contacts__list');
+var isNumberExist = document.querySelector('.already-exists-phone')
 
-var isNumberExist = elNewContactForm.querySelector('.already-exists-phone');
+
+function addContact(name, relationship, phone) {
+  contacts.push({
+    name: name,
+    relationship: relationship,
+    phone: phone
+  });
+}
+
+var elContactListItemTemplate = document.querySelector('#contact-list-item-template').content;
+
+function showContacts() {
+  elContactsList.innerHTML = '';
+  var elContactsFragment = document.createDocumentFragment();
+
+  for (var contact of contacts) {
+    var elNewContactsItem = elContactListItemTemplate.cloneNode(true);
+
+    elNewContactsItem.querySelector('.contact__name').textContent = contact.name;
+    elNewContactsItem.querySelector('.contact__relationship').textContent = contact.relationship;
+    elNewContactsItem.querySelector('.contact__phone-link').textContent = contact.phone;
+    elNewContactsItem.querySelector('.contact__phone-link').href = `tel:${contact.phone}`;
+
+    elContactsFragment.appendChild(elNewContactsItem);
+  }
+
+  elContactsList.appendChild(elContactsFragment);
+}
+
 
 // Form bo'lsa
 if (elNewContactForm) {
@@ -18,66 +46,61 @@ if (elNewContactForm) {
     var elNewContactNameInput = elNewContactForm.querySelector('.js-new-contact-name-input');
     var elNewContactRelationshipInput = elNewContactForm.querySelector('.js-new-contact-relationship-input');
     var elNewContactPhoneInput = elNewContactForm.querySelector('.js-new-contact-phone-input');
-    var isAvailable = contacts.findIndex(function(contact){
+    var phoneIndex = contacts.findIndex(function (contact) {
       return contact.phone === elNewContactPhoneInput.value.trim();
-    })
-    console.log(isAvailable);
+    });
 
-    if(isAvailable > -1){
+    // if (phoneIndex > -1) {
+    //   alert('Eee, borakan');
+    //   return;
+    // }
+    if (phoneIndex > -1) {
       elNewContactPhoneInput.classList.add('is-invalid');
       isNumberExist.classList.remove('d-none');
       return;
-    }else{
+    } else {
       elNewContactPhoneInput.classList.remove('is-invalid');
       isNumberExist.classList.add('d-none');
     }
 
-    addContact();
+    // Add contact to contacts array
+    addContact(
+      elNewContactNameInput.value.trim(),
+      elNewContactRelationshipInput.value.trim(),
+      elNewContactPhoneInput.value.trim()
+    );
+
+    // Reset inputs
+    elNewContactNameInput.value = '';
+    elNewContactRelationshipInput.value = '';
+    elNewContactPhoneInput.value = '';
+
     showContacts();
-
-    function addContact(){
-      contacts.push({
-        name: elNewContactNameInput.value.trim(),
-        relationship: elNewContactRelationshipInput.value.trim(),
-        phone: elNewContactPhoneInput.value.trim()
-      });
-
-      elNewContactNameInput.value = '';
-      elNewContactRelationshipInput.value = '';
-      elNewContactPhoneInput.value = '';
-    }
-
-    function showContacts(){
-      elContactsList.innerHTML = '';
-      var elContactsFragment = document.createDocumentFragment();
-
-      for (var contact of contacts) {
-        var elNewLi = document.createElement('li');
-        elNewLi.classList.add('list-group-item', 'p-3');
-
-        var elNewHeading = document.createElement('h3');
-        elNewHeading.classList.add('h5');
-        elNewHeading.textContent = contact.name;
-        elNewLi.appendChild(elNewHeading);
-
-        var elNewRelDiv = document.createElement('div');
-        elNewRelDiv.classList.add('mb-2');
-        elNewRelDiv.textContent = contact.relationship;
-        elNewLi.appendChild(elNewRelDiv);
-
-        var elNewPhoneLink = document.createElement('a');
-        elNewPhoneLink.classList.add('btn', 'btn-outline-success', 'btn-sm');
-        elNewPhoneLink.href = 'tel:' + contact.phone;
-        elNewPhoneLink.textContent = contact.phone;
-        elNewLi.appendChild(elNewPhoneLink);
-
-        elContactsFragment.appendChild(elNewLi);
-      }
-      elContactsList.appendChild(elContactsFragment);
-    }
-
-
-
   });
 }
-// Form submitda amal bajariladi
+
+// Yo'q bo'lgan kontaktnigina qo'shish
+/* var movies = [
+  {
+    title: 'Harry Potter',
+    year: 2000
+  },
+  {
+    title: 'James Bond',
+    year: 1970
+  },
+  {
+    title: 'Avengers',
+    year: 2012
+  }
+];
+
+var foundMovie = movies.find(function (movie) {
+  return movie.title === 'Avengers';
+});
+
+var foundMovieIndex = movies.findIndex(function (movie) {
+  return movie.title === 'Avengers';
+}); */
+
+// Qo'shilgan kontaktlarni o'chirib tashlash
