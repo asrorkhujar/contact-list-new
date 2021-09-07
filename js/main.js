af148fcef96342ed67b52018e4964a8d93337b80
coder-1990 <asrorakmalovich@gmail.com>
2021-09-07 18:42:58 +0500

    add delete function in JS

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ var elNewContactForm = document.querySelector('.js-new-contact-form');
 
 var elContacts = document.querySelector('.contacts');
 var elContactsList = elContacts.querySelector('.contacts__list');
-var isNumberExist = document.querySelector('.already-exists-phone')
+var isNumberExist = document.querySelector('.already-exists-phone');
+var elDeleteButton = document.querySelector('.delete-button');
 
 
 function addContact(name, relationship, phone) {
@@ -25,6 +26,7 @@ function showContacts() {
 
   for (var contact of contacts) {
     var elNewContactsItem = elContactListItemTemplate.cloneNode(true);
+    elNewContactsItem.querySelector('.contact').setAttribute('data-id', contact.phone);
 
     elNewContactsItem.querySelector('.contact__name').textContent = contact.name;
     elNewContactsItem.querySelector('.contact__relationship').textContent = contact.relationship;
@@ -50,10 +52,7 @@ if (elNewContactForm) {
       return contact.phone === elNewContactPhoneInput.value.trim();
     });
 
-    // if (phoneIndex > -1) {
-    //   alert('Eee, borakan');
-    //   return;
-    // }
+    //Agar bir xil raqam kiritilsa
     if (phoneIndex > -1) {
       elNewContactPhoneInput.classList.add('is-invalid');
       isNumberExist.classList.remove('d-none');
@@ -68,39 +67,25 @@ if (elNewContactForm) {
       elNewContactNameInput.value.trim(),
       elNewContactRelationshipInput.value.trim(),
       elNewContactPhoneInput.value.trim()
-    );
+      );
 
-    // Reset inputs
-    elNewContactNameInput.value = '';
-    elNewContactRelationshipInput.value = '';
-    elNewContactPhoneInput.value = '';
+      // Reset inputs
+      elNewContactNameInput.value = '';
+      elNewContactRelationshipInput.value = '';
+      elNewContactPhoneInput.value = '';
 
-    showContacts();
-  });
-}
-
-// Yo'q bo'lgan kontaktnigina qo'shish
-/* var movies = [
-  {
-    title: 'Harry Potter',
-    year: 2000
-  },
-  {
-    title: 'James Bond',
-    year: 1970
-  },
-  {
-    title: 'Avengers',
-    year: 2012
+      showContacts();
+    });
   }
-];
 
-var foundMovie = movies.find(function (movie) {
-  return movie.title === 'Avengers';
-});
 
-var foundMovieIndex = movies.findIndex(function (movie) {
-  return movie.title === 'Avengers';
-}); */
+  // Qo'shilgan kontaktlarni o'chirib tashlash
 
-// Qo'shilgan kontaktlarni o'chirib tashlash
+  elContactsList.addEventListener('click', (e) => {
+    if (e.target.matches('.delete-button')) {
+      var contactIndex = contacts.findIndex(contact => e.target.closest('.contact').dataset.id === contact.phone);
+      contacts.splice(contactIndex, 1);
+      console.log(contacts);
+      showContacts();
+    }
+  })
